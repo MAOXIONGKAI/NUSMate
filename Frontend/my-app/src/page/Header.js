3b30df49bf3900dc61a07c435f0d055df545da3d
Xiongkai <conan7153@gmail.com>
2024-05-29 00:13:34 +0800

    update Header.js
    Adjusted layout of header
    Add Account Menu, Notification Menu, and Chat Menu for better navigation experience
    Toggle Header layout and status based on whether user is logged in or not

--- Frontend/my-app/src/page/Header.js
+++ Frontend/my-app/src/page/Header.js
@@ -1,49 +1,92 @@
 import React from "react";
 import MainLogo from "../image/Main-Logo.png";
 import { Button } from "@mui/material";
+import NotificationMenu from "../component/NotificationMenu";
+import ChatMenu from "../component/ChatMenu";
+import AccountMenu from "../component/AccountMenu";
 import { Link } from "react-router-dom";
 import StyledButton from "../component/StyledButton";
 
-export default function Header() {
+function LoggedInHeader(prop) {
   return (
     <header className="header">
-      <img
-        className="header-logo"
-        src={MainLogo}
-        alt="Main Logo for NUSMate Main Page"
-      />
-      <nav className="header-nav-bar">
-        <ul className="header-nav-items">
-          <li>
-            <Button component={Link} to="/" variant="text">
-              Home
-            </Button>
-          </li>
-          <li>
-            <Button component={Link} to="/profile" variant="text">
-              Profile
-            </Button>
-          </li>
-          <li>
-            <Button component={Link} to="/discover" variant="text">
-              Discover
-            </Button>
-          </li>
-          <li>
-            <Button component={Link} to="/activity" variant="text">
-              Activity
-            </Button>
-          </li>
-        </ul>
-      </nav>
-
-      <StyledButton
-        text="Login / Register"
-        component={Link}
-        to="/login"
-        variant="contained"
-        style={{ color: "white", marginRight: "100px", fontSize: "16px" }}
-      />
+      <a href="/">
+        <img
+          className="header-logo"
+          src={MainLogo}
+          alt="Main Logo for NUSMate Main Page"
+        />
+      </a>
+      <div className="header-navigation">
+        <nav className="header-nav-bar">
+          <ul className="header-nav-items">
+            <li>
+              <Button component={Link} to="/discover" variant="text">
+                Discover
+              </Button>
+            </li>
+            <li>
+              <Button component={Link} to="/activity" variant="text">
+                Activity
+              </Button>
+            </li>
+          </ul>
+        </nav>
+        <nav className="header-userMenu">
+          <ul className="header-menuList">
+            <li>
+              <NotificationMenu />
+            </li>
+            <li>
+              <ChatMenu />
+            </li>
+            <li>
+              <AccountMenu
+                loggedIn={prop.loggedIn}
+                setLoggedIn={prop.setLoggedIn}
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
+  );
+}
+
+function LandingHeader(prop) {
+  return (
+    <header className="header">
+      <a href="/">
+        <img
+          className="header-logo"
+          src={MainLogo}
+          alt="Main Logo for NUSMate Main Page"
+        />
+      </a>
+        <StyledButton
+          text="Login / Register"
+          component={Link}
+          to="/login"
+          variant="contained"
+          style={{
+            color: "white",
+            marginLeft: "auto",
+            marginRight: "100px",
+            fontSize: "16px",
+          }}
+        />
+    </header>
+  );
+}
+
+export default function Header(prop) {
+  return (
+    <>
+      {prop.loggedIn ? (
+        <LoggedInHeader loggedIn={prop.loggedIn} setLoggedIn={prop.setLoggedIn} />
+      ) : (
+        <LandingHeader loggedIn={prop.loggedIn} setLoggedIn={prop.setLoggedIn} />
+      )}
+    </>
   );
 }
