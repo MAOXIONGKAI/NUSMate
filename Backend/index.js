48e2260f700ce08b0b5390a4ce8a4cf7a0b83713
Xiongkai <conan7153@gmail.com>
2024-06-07 21:39:06 +0800

    Update CORS Policy for development environment

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://nusmate.onrender.com"], // Allow requests from this origin
+    origin: ["http://localhost:3000", "https://nusmate.onrender.com", "https://nusmate-development.onrender.com"], // Allow requests from this origin
     methods: "GET,POST,PUT,DELETE", // Allowed methods
     allowedHeaders: "Content-Type,Authorization", // Allowed headers
   })
