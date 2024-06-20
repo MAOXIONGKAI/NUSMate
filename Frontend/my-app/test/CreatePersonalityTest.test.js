4d7a9bacaac5af19cc3ac86a35a1053b2a01f0ae
Xiongkai <conan7153@gmail.com>
2024-06-20 17:33:57 +0800

    Revert "Attempt to create test"
    This reverts commit 3dc673e027411c3c893991cf013cbe285f5c1ffe.

--- Frontend/my-app/test/CreatePersonalityTest.test.js
+++
@@ -1,5 +0,0 @@
-const CreatePersonalityTest = require('../src/data/CreatePersonalityTest');
-
-test("Create Personality Test should return an array of length 2", () => {
-    expect(CreatePersonalityTest()).toHaveLength(2);
-})
