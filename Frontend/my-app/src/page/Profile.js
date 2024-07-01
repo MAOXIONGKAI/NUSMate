2bafc148750c061a4b5bdd146b299bc6d9bb1603
Xiongkai <conan7153@gmail.com>
2024-07-01 14:21:01 +0800

    Update time format + bug fix for new user creating activity

--- Frontend/my-app/src/page/Profile.js
+++ Frontend/my-app/src/page/Profile.js
@@ -1,10 +1,33 @@
 import React from "react";
+import axios from "axios";
 import ProfilePage from "../component/ProfilePage";
 
+const backendURL = process.env.REACT_APP_BACKEND_URL;
+
 export default function Profile(prop) {
+  const updateProfile = async () => {
+    try {
+      const response = await axios.get(
+        `${backendURL}/api/profiles/email/${prop.profile.email}`,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      prop.setProfile(response.data);
+    } catch (error) {
+      console.log(
+        "Error when refreshing user profile info: " + error.response.data
+      );
+    }
+  };
+
+  React.useEffect(() => {
+    updateProfile();
+  }, []);
+
   return (
     <>
-      <ProfilePage profile={prop.profile} setProfile={prop.setProfile}/>
+      <ProfilePage profile={prop.profile} setProfile={prop.setProfile} />
     </>
   );
 }
