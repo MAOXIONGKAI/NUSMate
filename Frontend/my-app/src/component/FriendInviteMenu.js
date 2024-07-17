176c3e88f3f87c7a2ec4e1349642ccee632e9bed
Xiongkai <conan7153@gmail.com>
2024-07-17 14:03:08 +0800

    Implement Basic Layout for friend invite feature

---
+++ Frontend/my-app/src/component/FriendInviteMenu.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+
+import PersonIcon from "@mui/icons-material/Person";
+import { Paper } from "@mui/material";
+import GetAllJoinedParticipants from "../data/Participant/GetAllJoinedParticipants";
+import GetUserProfile from "../data/GetUserProfile";
+import FriendInviteList from "./FriendInviteList";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+  "& .MuiDialog-paper": {
+    borderRadius: "30px",
+    minWidth: "90%",
+    width: "90%",
+    minHeight: "90%",
+  },
+}));
+
+export default function FriendInviteMenu(prop) {
+  const {
+    open,
+    setOpen,
+    profile,
+    _id,
+    hostID,
+    hostName,
+    activityName,
+    pax,
+    openRemoveSuccess,
+    setOpenRemoveSuccess,
+    openRemoveFail,
+    setOpenRemoveFail,
+  } = prop;
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [participants, setParticipants] = React.useState([]);
+
+  React.useEffect(() => {
+    const getParticipants = async () => {
+      const participations = await GetAllJoinedParticipants(_id);
+      const profilePromises = participations?.map((participation) => {
+        return GetUserProfile(participation.participantID);
+      });
+      const hostProfile = await GetUserProfile(hostID);
+      const result =
+        hostProfile && profilePromises
+          ? await Promise.all([hostProfile, ...profilePromises])
+          : [];
+      setParticipants(result);
+    };
+    getParticipants();
+  }, [_id, hostID]);
+
+  return (
+    <React.Fragment>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle
+          sx={{ m: 0, p: 2, backgroundColor: "#EFF9FF", textAlign: "center" }}
+          id="customized-dialog-title"
+        >
+          <Typography
+            sx={{ display: "inline", color: "gray", fontSize: "24px" }}
+          >
+            Manage Participants in
+          </Typography>{" "}
+          {activityName}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+              gap: "30px",
+            }}
+          >
+            <Box
+              sx={{
+                width: "90%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "15px",
+              }}
+            >
+              <Box sx={{ display: "flex", gap: "10px" }}>
+                <Typography sx={{ color: "dimgray" }}>
+                  Participant List
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    color: participants.length === pax ? "red" : "green",
+                    margin: "0px",
+                    padding: "0px",
+                  }}
+                >
+                  <PersonIcon />
+                  <Typography variant="body2">
+                    ({participants.length}/{pax})
+                  </Typography>
+                </Box>
+              </Box>
+              <Paper
+                sx={{
+                  width: "100%",
+                  maxHeight: "400px",
+                  backgroundColor: "#F5F5F5",
+                  overflow: "auto",
+                  borderRadius: "20px",
+                }}
+              >
+                <FriendInviteList />
+              </Paper>
+            </Box>
+          </Box>
+        </DialogContent>
+      </BootstrapDialog>
+    </React.Fragment>
+  );
+}
